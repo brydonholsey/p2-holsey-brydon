9d515c670ae281cae2d18d4955a874ac5044230d
Brydon Holsey <brydon.holsey@ufl.edu>
2020-04-03 13:08:16 -0400

    Get API working

--- js/main.js
+++ js/main.js
@@ -16,3 +16,43 @@ function togglePrograms() {
 }
 
 upcomingPrograms.addEventListener('click', togglePrograms);
+
+
+
+//api
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var apiResult = JSON.parse(this.responseText);
+        
+
+        //Code Dealing With The API Data Goes Here
+
+        // var cityNameContainer = document.getElementById('city-name');
+        // cityNameContainer.innerHTML = apiResult.name;
+
+        // var weatherDescriptionContainer = document.getElementById('weather-description');
+        // weatherDescriptionContainer.innerHTML = apiResult.weather[0].description;
+
+        console.log(apiResult.heights[1].height);
+
+        var heightButtonOne = document.getElementById('height-morning');
+        var heightButtonTwo = document.getElementById('height-mid-day');
+        var heightButtonTwo = document.getElementById('height-evening');
+        var heightContainer = document.getElementById('height-container');
+
+        function showHeight() {
+            heightContainer.innerHTML = apiResult.heights[0].height;
+        }
+
+        heightButtonOne.addEventListener('click', showHeight);
+        
+        
+
+        
+
+
+    }
+};
+xmlhttp.open('GET', 'https://www.worldtides.info/api?heights&lat=33.768321&lon=-118.195617&key=fc3990db-3425-40fb-8450-97ad3c2cf8be', true);
+xmlhttp.send();
